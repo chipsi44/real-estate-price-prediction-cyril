e5d72574a5402b9e45decb888ec3871225666d16
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-01-30 14:37:55 +0100

    New Docker and requirements

--- deployement/app.py
+++ deployement/app.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, render_template
-from sklearn.linear_model import LinearRegression
-from model_preparation import model_training
 import pandas as pd
 import pickle
 
-try:
-    pandas_data = pd.read_csv('load_data.csv')
-    my_model = model_training(pandas_data)
-except Exception as e:
-    print("An error occurred while loading the data or training the model:", str(e))
+with open("deployement\model.pickle", "rb") as f:
+    model = pickle.load(f)
 
 app = Flask(__name__)
 
@@ -24,7 +19,7 @@ def predict():
             Number_bedrooms = float(request.form["Number_bedrooms"])
             Living_area = float(request.form["Living_area"])
             
-            prediction = my_model.predict([[locality, Number_bedrooms, Living_area]])
+            prediction = model.predict([[locality, Number_bedrooms, Living_area]])
             
             
             return render_template("index.html", prediction=format(prediction[0], ',.2f') + " €")
--- deployement/model_preparation.py
+++ deployement/model_preparation.py
@@ -1,9 +1,8 @@
 from sklearn.preprocessing import StandardScaler
-import numpy as np
-from scipy.stats import zscore
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-
+import pickle
+import pandas as pd
 def normalize_scale(df) :
     # Extract all columns except the second one (Price) and assign it to the variable X, and the second column (Price) is assigned to the variable y.
     X = df.drop('Price',axis=1)
@@ -32,4 +31,13 @@ def model_training(pandas_data, model = LinearRegression()) :
     model.fit(X_train, y_train)
     return model
 
-    
+
+
+def to_pickle():
+    # Your trained model
+    pandas_data = pd.read_csv('load_data.csv')
+    my_model = model_training(pandas_data)
+
+    # Export the model to a pickle file
+    with open('deployement/model.pickle', 'wb') as file:
+        pickle.dump(my_model, file)
